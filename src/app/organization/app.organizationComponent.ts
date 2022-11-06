1df7e8e92d7932a78e4bbadff9d568eb70f3faab
gitsjhare <sujit.jhare@gmail.com>
2022-11-06 14:55:01 +0530

    Updated Organization related changes

---
+++ src/app/organization/app.organizationComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Roshani-Organization',
+  templateUrl: './app.organizationComponent.html',
+  styleUrls: ['./app.organizationComponent.scss']
+})
+export class OrganizationComponent{}
